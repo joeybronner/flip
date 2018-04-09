a4fa3ddd7f1afdac4d224dbc2d3a4be86b421f2d
i312558 <joey.bronner@sap.com>
2018-04-09 11:22:58 +0200

    New method of connection

--- arduino/flip/flip.ino
+++ arduino/flip/flip.ino
@@ -1,8 +1,8 @@
-#include <MQTT.h>
 #include <PubSubClient.h>
-#include <PubSubClient_JSON.h>
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <BlynkSimpleEsp8266.h>
 
 // TODO
 
@@ -10,42 +10,37 @@
  * - no data during 1 minute, switch off the light
  */
 
-// Colors (RGB)
-const byte COLOR_BLACK = 0b000;
-const byte COLOR_RED = 0b100;
-const byte COLOR_GREEN = 0b010;
-const byte COLOR_BLUE = 0b001;
-const byte COLOR_MAGENTA = 0b101;
-const byte COLOR_CYAN = 0b011;
-const byte COLOR_YELLOW = 0b110;
-const byte COLOR_WHITE = 0b111;
 
 // MQTT
-const char *mqtt_server = "m10.cloudmqtt.com";
-const int mqtt_port = 17316;
-const char *mqtt_user = "flip-joey";
-const char *mqtt_pass = "helloworld";
-const String deviceId = "1";
+const char *mqtt_server = "m23.cloudmqtt.com";
+const int mqtt_port = 12358;
+const char *mqtt_user = "joey";
+const char *mqtt_pass = "password";
 
-// Broches RGB LED
-const int PIN_LED_R = D1;
-const int PIN_LED_G = D2;
-const int PIN_LED_B = D3;
+// Pins RGB LED
+const int PIN_LED_R = D5; // TODO: ADAPT
+const int PIN_LED_G = D7; // TODO: ADAPT
+const int PIN_LED_B = D6; // TODO: ADAPT
 
 // Button
-const int PIN_PUSH_BT = D4;
+const int PIN_PUSH_BT = D4; // TODO: ADAPT
 long buttonTimer = 0;
 long longPressTime = 2000;
 boolean buttonActive = false;
 boolean longPressActive = false;
 
 // WiFi credentials
-char ssid[] = "SAP-Guest";
-char pass[] = "";
+static WiFiClientSecure network;
+const char* KNOWN_SSID[]     = {"iPhone de Joey Bronner", "SAP-Guest", "SFR-97f0"};
+const char* KNOWN_PASSWORD[] = {"joeyjoey", "", "Q54CH9NNKU4A"};
+const int   KNOWN_SSID_COUNT = sizeof(KNOWN_SSID) / sizeof(KNOWN_SSID[0]);
+
+// Timer
+BlynkTimer timer;
 
 // Mode
 bool autoMode = true;
-String manuelStatus = "r";
+String manualStatus = "r";
 
 int val = 0;
 int incomingByte = 0;
@@ -60,42 +55,46 @@ void setup() {
     Serial.begin(9600);
     delay(200);
     Serial.println("");
-    Serial.println("NodeMCU Started");
+    Serial.println("Wemos Started");
+
+    // WiFi before launch
+    initWiFiConnection();
 
     // Initialize the LED pins as outputs
     pinMode(PIN_LED_R, OUTPUT);
     pinMode(PIN_LED_G, OUTPUT);
     pinMode(PIN_LED_B, OUTPUT);
 
-    pinMode(PIN_PUSH_BT, INPUT);
+    //pinMode(PIN_PUSH_BT, INPUT);
+
+    delay(100);
+    switchColor("g");
+    delay(3000);
+    switchColor("m");
+    delay(3000);
+
+    // Setup a function to be called every second
+    timer.setInterval(30000L, checkConnection);
 }
  
 void loop() {
-    if (WiFi.status() != WL_CONNECTED) {
-        connectWifi();
-        if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-            blinkColor(3000, COLOR_RED);
-            return;
-        } else {
-            displayColor(COLOR_GREEN);
-            delay(3000);
-            displayColor(COLOR_BLACK);
-            Serial.println("WiFi connected");
-        }
-    }
+
+    timer.run();
     
-    if (digitalRead(PIN_PUSH_BT) == HIGH) {
+    /*if (digitalRead(PIN_PUSH_BT) == HIGH) {
         Serial.println("Button pressed");
         switchColorManual();
         delay(200);
-    }
+    }*/
     
-    if (!client.connected()) {
+    if (!client.connected() && WiFi.status() == WL_CONNECTED) {
         Serial.println("Connecting to MQTT server");
         if (client.connect(MQTT::Connect("arduinoClient2").set_auth(mqtt_user, mqtt_pass))) {
             Serial.println("Connected to MQTT server");
             client.set_callback(callback);
-            client.subscribe("flip/devices/1/status");
+            String top = "flip/devices/1/status";
+            client.subscribe(top);
+            Serial.println("Subscribed on " + top);
         } else {
             Serial.println("Could not connect to MQTT server");   
         }
@@ -103,6 +102,16 @@ void loop() {
 
     if (client.connected() && autoMode) {
         client.loop();
+    }
+}
+
+void checkConnection () {
+    int timeout = millis();
+    if (WiFi.status() == WL_CONNECTED) {
+        Serial.println('Connected');
+    } else {
+        digitalWrite(BUILTIN_LED, 0);
+        initWiFiConnection();
     }
 }
 
@@ -121,6 +130,7 @@ void callback(const MQTT::Publish& pub) {
         }
         pub.payload_stream()->stop();
         String color = (char*)buf;
+        Serial.println("Color: " + color);
         switchColor(color);
     } else {
         Serial.println(pub.payload_string());
@@ -129,75 +139,149 @@ void callback(const MQTT::Publish& pub) {
 }
 
 /*
- * WiFi Utils
- */
-
-void connectWifi() {
-    Serial.print("Connecting to ");
-    Serial.print(ssid);
-    Serial.println("...");
-    WiFi.begin(ssid, pass);
-}
-
-/*
  * LED Utils
  */
 
 void switchColorManual() {
-    if (manuelStatus == "r") {
-        displayColor(COLOR_RED);
-        manuelStatus = "g";
-    } else if (manuelStatus == "g") {
-        displayColor(COLOR_GREEN);
-        manuelStatus = "r";
-    }/* else if (manuelStatus == "b") {
-        displayColor(COLOR_BLUE);
-        manuelStatus = "m";
-    } else if (manuelStatus == "m") {
-        displayColor(COLOR_MAGENTA);
-        manuelStatus = "c";
-    } else if (manuelStatus == "c") {
-        displayColor(COLOR_CYAN);
-        manuelStatus = "y";
-    } else if (manuelStatus == "y") {
-        displayColor(COLOR_YELLOW);
-        manuelStatus = "w";
-    } else if (manuelStatus == "w") {
-        displayColor(COLOR_WHITE);
-        manuelStatus = "r";
+    if (manualStatus == "r") {
+        switchColor(manualStatus);
+        manualStatus = "g";
+    } else if (manualStatus == "g") {
+        switchColor(manualStatus);
+        manualStatus = "r";
+    }/* else if (manualStatus == "b") {
+        switchColor(manualStatus);
+        manualStatus = "m";
+    } else if (manualStatus == "m") {
+        switchColor(manualStatus);
+        manualStatus = "c";
+    } else if (manualStatus == "c") {
+        switchColor(manualStatus);
+        manualStatus = "y";
+    } else if (manualStatus == "y") {
+        switchColor(manualStatus);
+        manualStatus = "w";
+    } else if (manualStatus == "w") {
+        switchColor(manualStatus);
+        manualStatus = "r";
     }*/
 }
 
 void switchColor(String c) {
     if (c == "r") 
-          displayColor(COLOR_RED);
+          displayColor(255, 0, 0);
     if (c == "g")
-          displayColor(COLOR_GREEN);
+          displayColor(0, 255, 0);
     if (c == "b")
-          displayColor(COLOR_BLUE);
+          displayColor(0, 0, 255);
     if (c == "m")
-          displayColor(COLOR_MAGENTA);
+          displayColor(80, 0, 80);
     if (c == "c")
-          displayColor(COLOR_CYAN);
+          displayColor(0, 255, 255);
     if (c == "y")
-          displayColor(COLOR_YELLOW);
+          displayColor(255, 255, 0);
     if (c == "w")
-          displayColor(COLOR_WHITE);
+          displayColor(255, 255, 255);
+    if (c == "o")
+          displayColor(0, 0, 0);
 }
 
-void displayColor(byte color) {
-  // Version anode commune
-  digitalWrite(PIN_LED_R, !bitRead(color, 2));
-  digitalWrite(PIN_LED_G, !bitRead(color, 1));
-  digitalWrite(PIN_LED_B, !bitRead(color, 0));
+void displayColor(int red, int green, int blue) {
+  #ifdef COMMON_ANODE
+    red = 255 - red;
+    green = 255 - green;
+    blue = 255 - blue;
+  #endif
+  analogWrite(PIN_LED_R, red);
+  analogWrite(PIN_LED_G, green);
+  analogWrite(PIN_LED_B, blue);  
 }
 
-void blinkColor(int t, byte color) {
+void blinkColor(int t, String c) {
   int times = t / 500;
   for (int i=0; i <= times; i++){
-      displayColor(color);
+      switchColor(c);
       delay(250);
-      displayColor(COLOR_BLACK);
+      switchColor("o");
       delay(250);
    }
+}
+
+void initWiFiConnection () {
+  digitalWrite(BUILTIN_LED, 0);
+  boolean wifiFound = false;
+  int i, n;
+
+  // ----------------------------------------------------------------
+  // Set WiFi to station mode and disconnect from an AP if it was previously connected
+  // ----------------------------------------------------------------
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+  Serial.println("Setup done");
+
+  // ----------------------------------------------------------------
+  // WiFi.scanNetworks will return the number of networks found
+  // ----------------------------------------------------------------
+  Serial.println(F("scan start"));
+  int nbVisibleNetworks = WiFi.scanNetworks();
+  Serial.println(F("scan done"));
+  if (nbVisibleNetworks == 0) {
+    Serial.println(F("no networks found. Reset to try again"));
+    while (true); // no need to go further, hang in there, will auto launch the Soft WDT reset
+  }
+
+  // ----------------------------------------------------------------
+  // if you arrive here at least some networks are visible
+  // ----------------------------------------------------------------
+  Serial.print(nbVisibleNetworks);
+  Serial.println(" network(s) found");
+
+  // ----------------------------------------------------------------
+  // check if we recognize one by comparing the visible networks
+  // one by one with our list of known networks
+  // ----------------------------------------------------------------
+  for (i = 0; i < nbVisibleNetworks; ++i) {
+    Serial.println(WiFi.SSID(i)); // Print current SSID
+    for (n = 0; n < KNOWN_SSID_COUNT; n++) { // walk through the list of known SSID and check for a match
+      if (strcmp(KNOWN_SSID[n], WiFi.SSID(i).c_str())) {
+        Serial.print(F("\tNot matching "));
+        Serial.println(KNOWN_SSID[n]);
+      } else { // we got a match
+        wifiFound = true;
+        break; // n is the network index we found
+      }
+    } // end for each known wifi SSID
+    if (wifiFound) break; // break from the "for each visible network" loop
+  } // end for each visible network
+
+  if (!wifiFound) {
+    Serial.println(F("no Known network identified. Reset to try again"));
+    while (true); // no need to go further, hang in there, will auto launch the Soft WDT reset
+  }
+
+  // ----------------------------------------------------------------
+  // if you arrive here you found 1 known SSID
+  // ----------------------------------------------------------------
+  Serial.print(F("\nConnecting to "));
+  Serial.println(KNOWN_SSID[n]);
+
+  // ----------------------------------------------------------------
+  // We try to connect to the WiFi network we found
+  // ----------------------------------------------------------------
+  WiFi.begin(KNOWN_SSID[n], KNOWN_PASSWORD[n]);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+
+  // ----------------------------------------------------------------
+  // SUCCESS, you are connected to the known WiFi network
+  // ----------------------------------------------------------------
+  digitalWrite(BUILTIN_LED, 1);
+  delay(150);
+  Serial.println(F("WiFi connected, your IP address is "));
+  Serial.println(WiFi.localIP());
 }
